1d6bad638985948e96a0fadd2061cad5c4e9cfdd
Nikica <kooh.kooh2@gmail.com>
2024-07-20 01:59:58 +0200

    1.0 Version of Incapable

---
+++ src/main/java/com/nikica/incapable/Incapable.java
@@ -0,0 +1,23 @@
+package com.nikica.incapable;
+
+import com.nikica.incapable.event.ModEvents;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import org.slf4j.LoggerFactory;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(Incapable.MOD_ID)
+public class Incapable
+{
+    public static final String MOD_ID = "incapable";
+    public static final Logger logger = LoggerFactory.getLogger(Incapable.class);
+
+    public Incapable(@NotNull IEventBus bus) {
+        // Register ourselves for server and other game events we are interested in
+        NeoForge.EVENT_BUS.addListener(ModEvents::onIncapable);
+    }
+}
---
+++ src/main/java/com/nikica/incapable/event/ModEvents.java
@@ -0,0 +1,48 @@
+package com.nikica.incapable.event;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.neoforge.common.ItemAbilities;
+import net.neoforged.neoforge.common.util.FakePlayer;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+
+public class ModEvents {
+    public static void onIncapable(PlayerEvent.BreakSpeed event) {
+        Player entity = event.getEntity();
+        Level level = entity.level();
+        BlockState blockState = event.getState();
+        ItemStack item = entity.getMainHandItem();
+        float destroySpeed = blockState.getDestroySpeed(entity.level(), event.getPosition().get());
+
+        // Ensure the entity is a real player and not in creative mode
+        if (entity == null || entity instanceof FakePlayer || entity.isCreative()) {
+            return;
+        }
+
+        if (destroySpeed >= 1.5F) {
+            boolean hasCorrectTool = item.isCorrectToolForDrops(blockState);
+            boolean hasDigAbility = item.canPerformAction(ItemAbilities.AXE_DIG) ||
+                                    item.canPerformAction(ItemAbilities.PICKAXE_DIG);
+
+            if (blockState.requiresCorrectToolForDrops()) {
+                if (hasCorrectTool) {
+                    return;
+                }
+                if (!hasDigAbility) {
+                    entity.hurt(level.damageSources().generic(), 2);
+                }
+            } else {
+                if (hasCorrectTool || hasDigAbility) {
+                    return;
+                }
+                entity.hurt(level.damageSources().generic(), 1);
+            }
+
+            event.setCanceled(true);
+        }
+    }
+}
